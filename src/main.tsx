65634e63e921f361eb591043a5a205b436b320f0
theMistletoe <katachiwotodomete@gmail.com>
2022-08-22 00:19:38 +0900

    service worker

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+// サービスワーカーを登録
+registerSW();
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    open: true,
+  },
+  plugins: [react(), VitePWA()],
   build: {
     emptyOutDir: true,
   },
